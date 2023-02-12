bf37345656de545aa98b0d48e101f320db7fcef1
ShepardWho <egorkudlai@gmail.com>
2023-02-12 18:50:16 +0200

    updated order for using with mongodb

--- models/user.js
+++ models/user.js
@@ -104,6 +104,14 @@ class User {
             .catch(err => console.log(err));
     }
 
+    getOrders() {
+        const db = getDb();
+        return db
+            .collection('orders')
+            .find({'user._id': new mongodb.ObjectId(this._id)})
+            .toArray();
+    }
+
     static findById(id) {
         const db = getDb();
         return db.collection('users')
